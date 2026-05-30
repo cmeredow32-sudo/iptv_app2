4d2541d63eee613d1062a9bdbefe664878c1a570
Agajan <agajan@example.com>
2026-05-30 15:12:37 +0500

    Hakyky turkmen-iptv (2) taslamasy ugradyldy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.turkmeniptv.zxcvbn"
+    applicationId = "com.turkmen.iptv"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
